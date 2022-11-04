9c04663e8e970b7bdc5bf74f1511e83c084995d2
JoseIgnacio9618 <joseignaciopardoperez@gmail.com>
2022-11-04 18:14:40 +0100

    Apuntes laravel9 modelo vista controlador 01/11/2022

--- 00-Frameworks/01- Laravel/02-Estructura de carpetas en proyecto laravel/prueba/routes/web.php
+++ 00-Frameworks/01- Laravel/02-Estructura de carpetas en proyecto laravel/prueba/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MultipleController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,34 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+
+// Las rutas contienen un link y una vista a la que llevarnos cuando el usuario llegue a ese link
+
+Route::get('/', HomeController::class);
+
+//version estandar
+/*
+Route::get('users', [MultipleController::class,'index']);
+
+Route::get('cursos', [MultipleController::class,'create']);
+
+Route::get('mensaje/{curso}', [MultipleController::class,'show']);
+*/
+
+
+
+// VERSION SIMPLIFICADA
+Route::controller(MultipleController::class)->group(function(){
+
+    Route::get('users', 'index');
+
+    Route::get('cursos', 'create');
+    
+    Route::get('mensaje/{curso}', 'show');
+
 });
+
+
+
+
+
